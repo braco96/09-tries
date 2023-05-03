e53a9920a462fc111a26edeb1ee07c494277540b
braco96 <bravocolladoluis@gmail.com>
2023-05-03 08:23:00 +0000

    commit 01/08

---
+++ src/main/java/com/braco96/algoritmosestructuras/tries/_00_trie/TrieNode.java
@@ -0,0 +1,13 @@
+package com.braco96.algoritmosestructuras.tries._00_trie;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrieNode {
+  Map<Character, TrieNode> children;
+  boolean isEndOfWord;
+
+  public TrieNode() {
+    children = new HashMap<Character, TrieNode>();
+  }
+}
